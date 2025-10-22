7971a836370bb6766208581d248ca35ac161333d
Daffansyah276 <daffansyaharya@gmail.com>
2025-10-22 15:31:58 +0700

    Add PersegiPanjang class with methods for calculating perimeter and area

---
+++ CLASS.py
@@ -0,0 +1,24 @@
+class PersegiPanjang:
+    def _init_(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+    
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
+    
+    def luas(self):
+        return self.panjang * self.lebar
+    
+    def _str_(self):
+        return f"Persegi Panjang {self.panjang} dan lebar {self.lebar}"
+    
+try:
+    panjang_input = float(input("Masukkan panjang (cm): "))
+    lebar_input = float(input("Masukkan lebar (cm): "))
+
+    pp = PersegiPanjang(panjang_input, lebar_input)
+    print("Keliling:", pp.keliling(), "cm")
+    print("Luas:", pp.luas(), "cm^²")
+
+except ValueError:
+    print("Input harus berupa angka.")
